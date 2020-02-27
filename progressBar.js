580fcf0a6fe121f0a0e0447e9baad1176046dd5f
Max <maxbendas86@gmail.com>
2020-02-27 22:58:33 +0200

    one day

---
+++ progressBar.js
@@ -0,0 +1,67 @@
+'use strict';
+
+class ProgressBar {
+  constructor(option = {}) {
+    const {
+      start = 0,
+      end = 100,
+      bg = 'green',
+      height = 20,
+      textColor = 'white',
+      border = '2px solid black',
+    } = option
+
+    this.start = start;
+    this.end = end;
+    this.bg = bg;
+    this.height = height;
+    this.textColor = textColor;
+    this.border = border;
+  }
+
+  init(selector) {
+    document.querySelector(selector).append(this.createProgressBar());
+  }
+
+  createProgressBar() {
+    const progressBar = document.createElement('div');
+    const bar = this.createBar();
+    progressBar.append(bar);
+    progressBar.style.width = '100%';
+    progressBar.style.border = this.border;
+    this.animateBar(bar);
+    return progressBar;
+
+  }
+
+  createBar() {
+    const bar = document.createElement('div');
+    bar.style.cssText = `
+      text-align: center;
+      background-color: ${this.bg};
+      height: ${this.height}px;
+      line-height: ${this.height}px;
+      color: ${this.textColor};
+      `;
+
+    this.stateProgress(bar);
+
+    return bar;
+  }
+
+  stateProgress(elem) {
+    elem.style.width = `${this.start}%`;
+    elem.textContent = `${this.start}%`
+  }
+
+  animateBar(bar) {
+    const animate = () => {
+      if (this.start < this.end) {
+        this.start++;
+        this.stateProgress(bar);
+        requestAnimationFrame(animate);
+      }
+    };
+    requestAnimationFrame(animate);
+  }
+}
